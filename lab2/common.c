bf38b13f9253a6d8bfb5abdd9fd914730ce68466
root <root@wifi-interno.fe.up.pt>
2016-10-05 21:49:44 +0100

    SET Packet is now sent correctly. Created makefile.

---
+++ lab2/common.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "common.h"
+
+volatile int STOP=FALSE;
+
+int send_message(int fd, char* buf, int buf_length) {
+        int written_chars = 0;
+        int res = 0;
+
+        while(written_chars < buf_length) {
+                res = write(fd,buf, buf_length);
+                if(res == 0) break;
+                written_chars += res;
+                printf("%d bytes written.\n", res);
+        }
+
+        return 0;
+}
+
+
+int read_message(int fd, char* msg, int* msg_len) {
+        int res = 0;
+        char buf[255];
+        *msg_len = 0;
+
+        while (STOP==FALSE) {   /* loop for input */
+                res = read(fd,buf,255); /* returns after x chars have been input */
+
+                if (buf[res-1]== 0)  STOP=TRUE;
+                printf("res: %d", res);
+                memcpy(msg+(*msg_len), buf, res);
+                (*msg_len) += res;
+        }
+
+        return 0;
+}
+
+
+void print_as_hexadecimal(char *msg, int msg_len) {
+        int i;
+        for(i = 0; i < msg_len; i++)
+                printf("%02X ", msg[i]);
+        fflush(stdout);
+}
---
+++ lab2/common.h
@@ -0,0 +1,25 @@
+#define BAUDRATE B9600
+#define FALSE 0
+#define TRUE 1
+#define FLAG 0x7E
+#define SEND 0x03
+#define RECEIVE 0x01
+#define SET 0x03
+#define UA 0x07
+
+
+/**
+* Write the message in buf, with buf_length length to fd.
+*/
+int send_message(int fd, char* buf, int buf_length);
+
+/**
+* Reads a message from fd and saves it in msg. msg_len is updated with the
+* new string length.
+*/
+int read_message(int fd, char* msg, int* msg_len);
+
+/**
+* Prints msg with msg length to the screen in hexadecimal format.
+*/
+void print_as_hexadecimal(char *msg, int msg_len);
