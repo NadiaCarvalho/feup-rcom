fbe85e99a67c2b972b32247ccdeacf70ef057717
Bernardo Belchior <bernardo.belchior1@gmail.com>
2016-10-10 15:02:55 +0100

    Reverted last commit.

--- lab2/writenoncanonical.c
+++ lab2/writenoncanonical.c
@@ -13,9 +13,23 @@
 #define MODEMDEVICE "/dev/ttyS1"
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 
-int parse_flag(char *packet) {
+int send_US_frame(int fd, int control_bit) {
+        char buf[6];
 
-        return 0;
+        buf[0] = FLAG;
+        buf[1] = SEND;
+        buf[2] = control_bit;
+        buf[3] = buf[1] ^ buf[2];
+        buf[4] = FLAG;
+	buf[5] = 0;
+
+        int buf_len = 6;
+
+	return send_message(fd, buf, buf_len);
+}
+
+int is_UA(char *msg) {
+	return (msg[0] == FLAG && msg[1] == SEND && msg[2] == UA && msg[3] == (msg[1] ^ msg[2]) && msg[4] == FLAG);
 }
 
 int main(int argc, char** argv)
@@ -88,9 +102,14 @@ int main(int argc, char** argv)
          */
 
         char msg[255] = "";
-        int msg_len;
-        read_message(fd, msg, &msg_len);
+       	int msg_len;
+	
+	while(!is_UA(msg)) {
+	     read_message(fd, msg, &msg_len);
         print_as_hexadecimal(msg, msg_len);
+}
+
+	printf("\nReceived UA.\n");
 
         if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
                 perror("tcsetattr");
